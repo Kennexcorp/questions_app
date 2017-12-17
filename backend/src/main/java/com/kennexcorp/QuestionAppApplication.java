b04f973757ed5fa885a1f72e2b5e4ff6e0697476
kennexcorp <ekeneoguikpu@yahoo.com>
2017-12-17 07:00:21 +0100

    Starter application for spring and vue js

---
+++ backend/src/main/java/com/kennexcorp/QuestionAppApplication.java
@@ -0,0 +1,12 @@
+package com.kennexcorp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestionAppApplication.class, args);
+	}
+}
